c4557a1309f928f00108c0afcdac832fe176b618
Tom <tom.lefevrebonzon@gmail.com>
2024-11-28 16:50:06 +0100

    script(DEMOWWOULDIAPPLICATION): caca

--- src/main/java/com/example/demowouldi/DemoWouldiApplication.java
+++ src/main/java/com/example/demowouldi/DemoWouldiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoWouldiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoWouldiApplication.class, args);
+        SpringApplication.run(DemoWouldiApplicahhjkkkjktion.class, args);
     }
 
 }
