033135e78d8163608dc7761b0526fcd14bcb6078
Tom <tom.lefevrebonzon@gmail.com>
2024-11-28 16:51:26 +0100

    featcacazobchatte5

--- src/main/java/com/example/demowouldi/DemoWouldiApplication.java
+++ src/main/java/com/example/demowouldi/DemoWouldiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoWouldiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoWouldiApplicahhjkkkjktion.class, args);
+        SpringApplication.run(DemoWouldiApplinkklllll5cahhjkkkjktion.class, args);
     }
 
 }
