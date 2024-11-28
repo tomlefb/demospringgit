a310f3b879851491dd26e0f18b6c391e1e4ec99f
Tom <tom.lefevrebonzon@gmail.com>
2024-11-28 16:47:08 +0100

    first commit

---
+++ src/main/java/com/example/demowouldi/DemoWouldiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demowouldi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWouldiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWouldiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demowouldi/DemoWouldiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demowouldi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWouldiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
